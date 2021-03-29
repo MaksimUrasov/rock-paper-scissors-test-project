baccd21fef01053fe859f3050d17db3052ee383d
maksim <maksim.urasov@gmail.com>
2021-03-29 20:07:08 +0300

    finished game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const arrayOfGestures = ['Rock','Paper','Scissors'];
 
 const win = "You Win!"
@@ -5,9 +6,12 @@ const loose = "You Loose!"
 const pr = "Paper beats Rock"
 const rs = "Rock beats Scissors"
 const sp = "Scissors beats Paper"
-const same = "Same figures stay as they are"
 const eq = "Noone wins."
+const same = "siblings do not fight."
 const er = "Seems You have mistyped, please try again."
+
+const scorePElement = document.getElementById('resultScore')
+const logPElement = document.getElementById('resultLog')
 
 
 function computerPlay() {
@@ -15,7 +19,7 @@ function computerPlay() {
 }
 
 function alertTurnResult(winOrLoose, plSel, compSel, rule, gameNumber) {
-    alert(`Game round ${gameNumber} out of 5.\n \n${winOrLoose} \nYour input is ${plSel}, while computer chose ${compSel}. \nThe rule is that ${rule}.`)
+    alert(`Round ${gameNumber} out of 5.\n \n${winOrLoose} \nYour input is ${plSel}, while computer chose ${compSel}. \nThe rule is that ${rule}.`)
 }
 
 function alertMistype(winOrLoose, gameNumber) {
@@ -23,56 +27,77 @@ function alertMistype(winOrLoose, gameNumber) {
 }
 
 function playRound (playerSelection, computerSelection, numberOfGames) {
-    let yourScore = 0;
-    let computersScore = 0;
+    let roundResult = [];
 
     if (playerSelection.toLowerCase().trim() == "rock" && computerSelection == "Paper") {
         alertTurnResult(loose, playerSelection, computerSelection, pr ,numberOfGames)
-        return (-1);
+        return roundResult = [playerSelection, computerSelection, -1]
 
     } else if (playerSelection.toLowerCase().trim() == "rock" && computerSelection == "Scissors") {
         alertTurnResult(win, playerSelection, computerSelection, rs ,numberOfGames)
-        return (1);
+        return roundResult= [playerSelection, computerSelection, 1];
 
     } else if (playerSelection.toLowerCase().trim() == "paper" && computerSelection == "Rock") {
         alertTurnResult(win, playerSelection, computerSelection, pr ,numberOfGames)
-        return (1);
+        return roundResult= [playerSelection, computerSelection, 1];
+
 
     } else if (playerSelection.toLowerCase().trim() == "paper" && computerSelection == "Scissors") {
         alertTurnResult(loose, playerSelection, computerSelection, sp ,numberOfGames)
-        return (-1);
+        return roundResult=[playerSelection, computerSelection, -1];
 
     } else if (playerSelection.toLowerCase().trim() == "scissors" && computerSelection == "Rock") {
         alertTurnResult(loose, playerSelection, computerSelection, rs ,numberOfGames)
-        return (-1);
+        return roundResult=[playerSelection, computerSelection, -1];
 
     } else if (playerSelection.toLowerCase().trim() == "scissors" && computerSelection == "Paper") {
         alertTurnResult(win, playerSelection, computerSelection, sp ,numberOfGames)
-        return (1);
+        return roundResult=[playerSelection, computerSelection, 1];
 
     } else if (playerSelection.toLowerCase().trim() == computerSelection.toLowerCase() ) {  
         alertTurnResult(eq, playerSelection, computerSelection, same ,numberOfGames)
-        return (0);
+        return roundResult= [playerSelection, computerSelection, 0];
 
     } else {
-        console.log(playerSelection.toLowerCase().trim(), computerSelection.toLowerCase());
         alertMistype(er, numberOfGames)
-        return (-1);
+        return roundResult = [playerSelection, computerSelection, -1]
     }
     
  }
 
 
 function game() {
-    for (i=1; i<6; i++) {
-        let finalResult = playRound( prompt("please type 'Rock', 'Paper' or 'Scissors'"), computerPlay(), i);
+    let finalResultScore = 0;
+    let finalResultLog = [];
 
-        if (i==5 && finalResult>0 ) {
-            alert(`YOU ARE THE WINNER!!!!!! \n\mPlease press Start to play again :)`)
-        } else if (i==5 && finalResult<0 ) {
-            alert(`Ooooh, computer has won this tournament, but dont worry you can try again :)`)
-        } else if (i==5 && finalResult==0 )  {
-            alert (`DRAW! \n Sometimes that happens, but dont worry you can try again :) `)
-        }
+ 
+
+    for (i=1; i<6; i++) {
+        let roundResultArray = playRound( prompt("please type 'Rock', 'Paper' or 'Scissors'"), computerPlay(), i);
+        // console.log(roundResultArray);
+        finalResultLog.push( "<br>", i," round. ", roundResultArray)
+        // console.log(finalResultLog);
+        let roundNumericalResult = parseInt(roundResultArray.slice(-1));
+        finalResultScore += roundNumericalResult;
+
+            
+
     }
+    
+    if (finalResultScore>0 ) {
+        alert(`YOU ARE THE WINNER!!!!!! \n\mPlease press Start to play again :)`)
+        scorePElement.innerHTML = `The final score: ${finalResultScore}.`
+        logPElement.innerHTML = `Game log: ${finalResultLog}.`
+    } else if (finalResultScore<0 ) {
+        alert(`Ooooh, computer has won this tournament, but dont worry you can try again :)`)
+        scorePElement.innerHTML = `The score: ${finalResultScore}.`
+        logPElement.innerHTML = `Game log: ${finalResultLog}.`
+    } else if (finalResultScore==0 )  {
+        alert (`DRAW! \n Sometimes that happens, but dont worry you can try again :) `)
+        scorePElement.innerHTML = `The score: ${finalResultScore}.`
+        logPElement.innerHTML = `Game log: ${finalResultLog}.`
+    }
+
+
+    
 }
